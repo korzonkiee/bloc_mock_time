1cf304b33b7a7ba762301f7cf83d83159cc97dd2
Felix Angelov <felangelov@gmail.com>
2020-12-15 11:50:34 -0600

    fix: use withClock to mock time

--- test/counter_cubit_test.dart
+++ test/counter_cubit_test.dart
@@ -1,19 +1,20 @@
 import 'package:bloc_mock_time/counter.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:clock/clock.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  withClock(
-    Clock.fixed(DateTime(1970, 1, 1)),
-    () {
-      blocTest<CounterCubit, int>(
-        'should increment',
-        build: () => CounterCubit(),
-        act: (cubit) => cubit.increment(),
-        expect: [
-          1,
-        ],
-      );
-    },
-  );
+  test('should increment with mocked time', () {
+    withClock(
+      Clock.fixed(DateTime(1970, 1, 1)),
+      () {
+        runBlocTest<CounterCubit, int>(
+          'should increment',
+          build: () => CounterCubit(),
+          act: (cubit) => cubit.increment(),
+          expect: [1],
+        );
+      },
+    );
+  });
 }
